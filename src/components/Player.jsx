2da3957ad74bdfbf83d9f3533afe61a8172c3fd2
Mohamed Nasr <mnasr0868@gmail.com>
2026-09-07 16:00:10 +0300

    Add Player component for managing player names and symbols

---
+++ src/components/Player.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const handleEdit = () => {
+    setIsEditing(oldIsEditing=>!oldIsEditing); //false => true
+    // setIsEditing(oldIsEditing=>!oldIsEditing); //false => true
+  };
+  let playerName = <span className="player-name">{name}</span>;
+  if (isEditing) {
+    playerName = <input type="text" required value={name} />;
+  }
+  return (
+    <li>
+      <span className="player">
+        {playerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEdit}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
