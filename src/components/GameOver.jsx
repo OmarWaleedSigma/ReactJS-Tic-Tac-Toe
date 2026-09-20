686bd4cec840356a70bcd1adbaecb2dda64b2d7a
= <morawaleed2017@gmail.com>
2026-09-20 15:45:25 +0300

    Enhance GameOver component messaging to celebrate the winner and clarify draw scenario

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -3,9 +3,18 @@ export default function GameOver({ winner, onReset }) {
     <div id="game-over">
       <h2>Game Over</h2>
       {winner && (
-        <p style={{ textTransform: "capitalize" }}>{winner} won the game 🥇🏆!</p>
+        <>
+        <p style={{ textTransform: "capitalize" }}>a new champion has arrived: "{winner}" !</p>
+        <p style={{ textTransform: "capitalize" }}>Congrats🥇🏆!</p>
+        </>
       )}
-      {!winner && <p style={{ textTransform: "capitalize" }}>it's a draw...😑</p>}
+      {!winner && (
+        <>
+          <p style={{ textTransform: "capitalize" }}>
+            no one won this time...😔
+          </p>
+        </>
+      )}
       <p>
         <button onClick={onReset}>Rematch 🔁!</button>
       </p>
